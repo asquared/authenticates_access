083885025a90e01ed4137843e61db9838aad4a91
Andrew Armenia <andrew@asquaredlabs.com>
2009-07-02 20:14:46 +0000

    kludge job so fixtures work on both 2.2 and 2.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -51,7 +51,11 @@ end
 
 class ActiveSupport::TestCase
   # 2.3.2 seems to need this?
-  include ActiveRecord::TestFixtures
+  begin 
+    include ActiveRecord::TestFixtures
+  rescue NameError
+    puts "You appear to be using a pre-2.3 version of Rails. No need to include ActiveRecord::TestFixtures..."
+  end
 
   self.fixture_path = File.join(File.dirname(__FILE__), "fixtures")
 
